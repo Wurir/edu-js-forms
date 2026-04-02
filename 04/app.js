5c182e4f9f71b2340a6877cab04eb9da514054c4
WurirMacOs <wurir1994@gmail.com>
2026-04-02 23:15:15 +0200

    do color change

--- 04/app.js
+++ 04/app.js
@@ -3,6 +3,14 @@ document.addEventListener('DOMContentLoaded', init);
 function init() {
     const boxElement = document.querySelector('.box');
     setBoxShadow(boxElement, '#000000');
+
+    const inputColor = document.querySelector('input[type="color"]')
+    const inputRange = document.querySelector('input[type="range"]')
+
+    inputColor.addEventListener('change', function (e) {
+        const color = e.target.value
+        setBoxShadow(boxElement, color)
+    })
 }
 
 function setBoxShadow(element, colorInHex, opacity = 1) {
@@ -19,7 +27,7 @@ function setBoxShadow(element, colorInHex, opacity = 1) {
 
 function getChannelColor(colorInHex, channelName) {
     let start;
-    switch(channelName) {
+    switch (channelName) {
         case 'red':
             start = 1;
             break;
@@ -34,7 +42,7 @@ function getChannelColor(colorInHex, channelName) {
     const channelColorHex = colorInHex.substr(start, 2);
     const channelColorDec = parseInt(channelColorHex, 16);
 
-    return channelColorDec; 
+    return channelColorDec;
 }
 
 
