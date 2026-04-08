58fe82331df3e1705085b971dfd57e49d2a4ae36
Bartosz Bogdanowicz <wurir1994@gmail.com>
2026-04-08 14:50:52 +0200

    refactor 05, add validateForm fn, fields with inputs to validate

--- 05/app.js
+++ 05/app.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', init)
 
-
 const msgList = document.querySelector('.messages')
 
 function init() {
@@ -8,73 +7,56 @@ function init() {
     const formEl = document.querySelector('form')
     if (formEl) {
         formEl.addEventListener('submit', handleSubmit)
-
     }
 
+    function handleSubmit(e) {
+        e.preventDefault()
 
+        msgList.innerHTML = ''
+
+        const fields = [
+            { name: 'firstName', label: 'imię', required: true },
+            { name: 'lastName', label: 'nazwisko', required: true },
+            { name: 'street', label: 'ulica', required: true },
+            { name: 'houseNumber', label: 'numer budynku', type: 'number', required: true },
+            { name: 'flatNumber', label: 'numer mieszkania', type: 'number', required: false },
+            { name: 'zip', label: 'kod pocztowy', pattern: '[0-9]{2}-[0-9]{3}', required: true },
+            { name: 'city', label: 'miejscowość', required: true },
+            { name: 'voivodeship', label: 'województwo', required: true },
+        ]
+
+        const errors = []
+
+        fields.forEach(function (field) {
+            validateForm(formEl, field, errors)
+        })
+
+        if (errors.length > 0) {
+            displayErrors(errors)
+        } else {
+            alert('Dane zostały wysłane')
+        }
+
+    }
 }
 
+function validateForm(formElement, field, errorsList) {
+    const value = formElement.elements[field.name].value
 
-function handleSubmit(e) {
-    e.preventDefault()
-
-    msgList.innerHTML = ''
-
-    const errors = []
-
-    const firstName = e.target.elements.firstName.value
-    const lastName = e.target.elements.lastName.value
-    const street = e.target.elements.street.value
-    const houseNumber = e.target.elements.houseNumber.value
-    const flatNumber = e.target.elements.flatNumber.value
-    const zip = e.target.elements.zip.value
-    const city = e.target.elements.city.value
-    const voivodeship = e.target.elements.voivodeship.value
-
-    if(isEmpty(firstName)){
-        errors.push('Dane w polu imię są niepoprawne')
+    if (field.required && isEmpty(value)) {
+        errorsList.push('Dane w polu ' + field.label + ' są niepoprawne')
+        return
     }
-
-    if(isEmpty(lastName)){
-        errors.push('Dane w polu nazwisko są niepoprawne')
+    if (field.pattern && !isEmpty(value)) {
+        const regex = new RegExp(field.pattern)
+        if (!regex.test(value)) {
+            errorsList.push('Dane w polu ' + field.label + ' są niepoprawne')
+        }
     }
-
-    if(isEmpty(street)){
-        errors.push('Dane w polu ulica są niepoprawne')
-    }
-
-    if(isEmpty(houseNumber)){
-        errors.push('Dane w polu numer budynku są niepoprawne')
-    }
-
-    if(isEmpty(flatNumber)){
-        errors.push('Dane w polu numer mieszkania są niepoprawne')
-    }
-
-    const zipPattern = e.target.zip.pattern
-    const regex = new RegExp(zipPattern)
-    if(!regex.test(zip)){
-        errors.push('Dane w polu kod pocztowy są niepoprawne')
-    }
-
-    if(isEmpty(city)){
-        errors.push('Dane w polu miejscowość są niepoprawne')
-    }
-
-    if(isEmpty(voivodeship)){
-        errors.push('Dane w polu województwo są niepoprawne')
-    }
-    
-    if(errors.length > 0){
-        displayErrors(errors)
-    }else {
-        alert('Dane zostały wysłane')
-    }
-
 }
 
-function displayErrors(errList){
-    errList.forEach(function(error){
+function displayErrors(errList) {
+    errList.forEach(function (error) {
         const liEl = document.createElement('li')
         liEl.innerText = error
         liEl.style.color = 'red'
@@ -82,6 +64,6 @@ function displayErrors(errList){
     })
 }
 
-function isEmpty(value){
+function isEmpty(value) {
     return value === ''
 }
